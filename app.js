1da7ce1312bf783a3cc3e2302155bcbf1782044f
JackOliveira1 <jackoliveira14adm10@gmail.com>
2023-10-24 17:35:45 -0300

    Routing requests and redirecting requests

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+
+    const url = req.url;
+    const method = req.method;
+
+
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Segunda Pagina!!!</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Enviar</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end()
+    }
+
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Primeira Pagina</title></head>');
+    res.write('<body><h1>Primeira pagina Node</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
