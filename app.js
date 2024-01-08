c247c823bd4d465c62a6d3371aa9b8e49385415c
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-01-08 14:46:52 -0300

    Filtering Paths

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin', adminRouter);
 app.use(shopRouter);
-app.use(adminRouter);
+
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not Found</h1>');
+});
 
 app.listen(3000);
