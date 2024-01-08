04deeb95d6ff18a2660131b3ad9c4c9308bb7f4e
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-01-08 13:03:15 -0300

    Limiting Middleware execution to POST request

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
-server.listen(3000);
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
