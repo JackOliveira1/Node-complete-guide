fc8757dcd2488c05b10fafd97e496a059c98d6ef
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-01-21 12:07:37 -0300

    Sharing data acroo requests & users

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) =>{
     res.status(404).sendFile(path.join(__dirname,'views', 'page-not-found.html'));
