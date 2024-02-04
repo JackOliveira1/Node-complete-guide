3a8ea8b9e292734760dfd60be77d55940806a035
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-02-04 11:56:13 -0300

    Handlebars and EJS

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,29 @@
+const path = require('path');
+
 const express = require('express');
-const router = express.Router();
-const path  = require('path');
+
 const rootDir = require('../util/path');
 
+const router = express.Router();
 
 const products = [];
 
-// GET
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-// POST
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title })
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-// module.exports = router;
 exports.routes = router;
 exports.products = products;
