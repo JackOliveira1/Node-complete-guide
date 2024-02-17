7530b20fee7cf8f66b9760a0e1be0422ba9274d5
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-02-17 19:24:16 -0300

    Create shop structure, navigation, registering routes, storing product Data, displaying product Data, editing and deleting products

--- data/products.js
+++ data/products.js
@@ -1,1 +1,1 @@
-[{"title":"teste"},{"title":"segundo"}]
+[{"title":"Lente ","imageUrl":"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg","description":"Lente de câmera","price":"1000"}]
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,16 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/prducts', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
