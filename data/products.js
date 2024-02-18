995165df5c252e569032788633d3784475b2f578
JackOliveira1 <jackoliveira14adm10@gmail.com>
2024-02-18 13:21:23 -0300

    Adding product ID to the path, extracting dynamic params, loading product detail Data, rendering the product detail view

--- data/products.js
+++ data/products.js
@@ -1,1 +1,1 @@
-[{"title":"Lente ","imageUrl":"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg","description":"Lente de câmera","price":"1000"}]
+[{"id":"65486","title":"Lente ","imageUrl":"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg","description":"Lente de câmera","price":"1000"}]
